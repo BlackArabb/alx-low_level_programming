b7a0371a7a55f602bb6feecb9a00d659d9e82f85
BlackArabb <alghareeb.mk@gmail.com>
2023-02-16 17:28:17 -0800

    Low level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+/* A program to print output without using "printf" function*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
