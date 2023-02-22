9fe0466a81d14d4fda0921b0a9548c89858e16db
BlackArabb <alghareeb.mk@gmail.com>
2023-02-21 19:07:30 -0800

    tough

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
-#include <main.h>
+#include "main.h"
 
 /**
  *main: a program to output putchar
  *RETURN 0
  */
 
+
 int main()
 {
-	_putchar("_putchar");
+	_putchar("\_putchar\n");
 	return (0);
 }
